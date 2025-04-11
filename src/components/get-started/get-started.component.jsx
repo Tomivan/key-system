2093c32aa216f22fed1ee22947bc77d8aa288ba3
PeterTomivan <gracejunkie20@gmail.com>
2025-04-11 15:42:11 +0100

    gradient circle added

--- src/components/get-started/get-started.component.jsx
+++ src/components/get-started/get-started.component.jsx
@@ -8,14 +8,18 @@ const GetStarted = () => {
                 <div className="content">
                     <h2>Ready to get started?</h2>
                     <p>Shoot us a Mail</p>
-                    <p> Join numerous corporations, organizations, and businesses as they <br /> 
-                        scale their teams, tap into new market opportunities and build <br /> 
+                    <p> Join numerous corporations, organizations, and businesses as they 
+                        scale their teams, tap into new market opportunities and build 
                         innovative products with KeySystem Technology</p>
-                    <p>Join thousands of students who are ready to learn new skills or take <br /> 
-                    their career to the next level by enrolling in one of our training <br /> programs today.</p>
+                    <p>Join thousands of students who are ready to learn new skills or take  
+                    their career to the next level by enrolling in one of our training programs today.</p>
                     <button className='send-mail'>Send us a Mail Now</button>
                 </div>
-                <div className="right-design"></div>
+                <div class="circle-container">
+                    <div class="circle circle-1"></div>
+                    <div class="circle circle-2"></div>
+                    <div class="circle circle-3"></div>
+                </div>
             </div>
         </div>
     )
